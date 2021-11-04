120ed8c5280413daa24ac3eccc7e9683241ec8df
Priya X. Pramesi <ppramesi@visi8.com>
2021-11-04 17:08:04 +0700

    fixed some stuff

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -12,6 +12,14 @@
                 </svg>
             </router-link>
         </div>
+        <div ref="arrowTwo" class="arrow-container">
+            <router-link to="/toilet/">
+                <div class="text-white">Toilet view</div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="arrow" fill="white" viewBox="0 0 24 24" stroke="black">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z" />
+                </svg>
+            </router-link>
+        </div>
     </div>
 </template>
 
@@ -24,6 +32,7 @@ export default {
         const arrowTwo = this.$refs.arrowTwo
         this.marzObj = createMarzipano(elem, '/assets/scenes/cabin/cabin.jpg')
         this.hotSpots.arrowOne = this.marzObj.scene.hotspotContainer().createHotspot(arrowOne, this.coords.arrowOne)
+        this.hotSpots.arrowTwo = this.marzObj.scene.hotspotContainer().createHotspot(arrowTwo, this.coords.arrowTwo)
     },
     data(){
         return {
@@ -35,6 +44,10 @@ export default {
                 arrowOne: {
                     yaw: -0.100649973562619,
                     pitch: 0.05547168452443522,
+                },
+                arrowTwo: {
+                    yaw: 2.9011735977849913,
+                    pitch: -0.05547168452443522,
                 }
             }
         }
